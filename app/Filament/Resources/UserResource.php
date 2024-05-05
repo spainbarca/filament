cb58ce4f0c541b1000fa5267118d911c613e3fa1
Noah <practicasjaguar@gmail.com>
2024-05-05 00:51:36 -0500

    Asignar roles a usuarios

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Date;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->email()
+                    ->required(),
+                TextInput::make('password')
+                    ->password()
+                    ->hiddenOn('edit')
+                    ->required(),
+                Select::make('roles')->multiple()->relationship('roles','name')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                TextColumn::make('name'),
+                TextColumn::make('email'),
+                TextColumn::make('email_verified_at'),
+                TextColumn::make('roles.name')
+            ])
+            ->filters([
+                Tables\Filters\Filter::make('verified')
+                    ->query(fn (Builder $query) : Builder => $query->whereNotNull('email_verified_at')),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\Action::make('Verify')
+                    ->icon('heroicon-m-check-badge')
+                    ->action(function (User $user){
+                        $user->email_verified_at = Date('Y-m-d H:i:s');
+                        $user->save();
+                    }),
+                Tables\Actions\Action::make('Unverify')
+                    ->icon('heroicon-m-x-circle')
+                    ->action(function (User $user){
+                        $user->email_verified_at = null;
+                        $user->save();
+                    }),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
